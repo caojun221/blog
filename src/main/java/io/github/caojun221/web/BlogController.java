c9cf31ffcc9590b584f8c07a1e3069a2bf42baa2
caojun221 <caojun07@gmail.com>
2016-12-31 11:47:39 +0900

    tmp

--- src/main/java/io/github/caojun221/web/BlogController.java
+++ src/main/java/io/github/caojun221/web/BlogController.java
@@ -4,15 +4,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.sun.deploy.net.HttpResponse;
-import com.sun.deploy.util.SystemUtils;
 
 import io.github.caojun221.domain.Article;
 import io.github.caojun221.service.BlogService;
